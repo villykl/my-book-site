97a006539340efae5232dcb0a7c4d9861b830d10
Vera Klimoshenko <villykli@gmail.com>
2026-04-11 05:46:17 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,9 @@
-const CACHE = "book-app-v2";
-
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => {
-        if (k !== CACHE) return caches.delete(k);
-      }))
-    )
-  );
-  self.clients.claim();
-});
-
-/* УМНЫЙ КЭШ */
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        let copy = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.open("book").then(cache=>{
+      return cache.addAll([
+        "index.html"
+      ]);
+    })
   );
 });
