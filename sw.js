5e8c84679e5cc2301446ca31c23bd4359b43186b
Vera Klimoshenko <villykli@gmail.com>
2026-04-27 19:37:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,29 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("book").then(cache=>{
-      return cache.addAll([
-        "index.html"
-      ]);
-    })
-  );
-});
+<script>
+function initBook() {
+    const book = new St.PageFlip(document.getElementById("book"), {
+        width: 490,
+        height: 640,
+        size: "stretch",
+        showCover: true,
+        maxShadowOpacity: 0.75,
+        flippingTime: 900,
+        drawShadow: true,
+        useMouseEvents: true,
+        swipeDistance: 20,
+        mobileScrollSupport: true
+    });
+
+    book.loadFromHTML(document.querySelectorAll(".page"));
+
+    setTimeout(() => {
+        document.body.classList.add("ready");
+    }, 400);
+}
+
+/* 🔥 ГЛАВНЫЙ ФИКС (ключевой момент) */
+if (document.readyState === "complete") {
+    initBook();
+} else {
+    window.addEventListener("load", initBook);
+}
+</script>
